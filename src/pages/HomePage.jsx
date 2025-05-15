36b01617e63b4ff5150a4126cda980260cf3b380
Andrea <andrew.ryan.r.a@gmail.com>
2025-05-15 19:51:40 +0200

    Project folders refactor

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,21 @@
-import ProfileInfo from "../components/navigation/profileInfo/ProfileInfo.jsx";
-import UsefullProfileLinkComponent from "../components/navigation/usefullProfileLinks/UsefullProfileLinkComponent.jsx";
+import ProfileInfo from "../components/profileInfo/ProfileInfo.jsx";
+import UsefullProfileLinkComponent from "../components/usefullProfileLinks/UsefullProfileLinkComponent.jsx";
 import ThreeColsLayout from "../layouts/ThreeColsLayout.jsx";
 import NewsContainer from "../components/newsContainer/NewsContainer.jsx";
 
 const HomePage = () => {
-  return (
-    <ThreeColsLayout>
-      <div className="col col-12 col-md-6 col-lg-3">
-        <ProfileInfo />
-        <UsefullProfileLinkComponent />
-      </div>
-      <div className="col col-12 col-md-6 col-lg-5">Colonna da 5</div>
-      <div className="col col-12 col-md-6 col-lg-4">
-        <NewsContainer />
-      </div>
-    </ThreeColsLayout>
-  );
+    return (
+        <ThreeColsLayout>
+            <div className="col col-12 col-md-6 col-lg-3">
+                <ProfileInfo />
+                <UsefullProfileLinkComponent />
+            </div>
+            <div className="col col-12 col-md-6 col-lg-5">Colonna da 5</div>
+            <div className="col col-12 col-md-6 col-lg-4">
+                <NewsContainer />
+            </div>
+        </ThreeColsLayout>
+    );
 };
+
+export default HomePage;
