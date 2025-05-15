ed12c22f771b19403d114de48146b2bd78b2f961
Danilo Scano <daniloscano@gmail.com>
2025-05-15 20:59:11 +0200

    added opportunity component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import ProfileInfo from "../components/profileInfo/ProfileInfo.jsx";
 import UsefullProfileLinkComponent from "../components/usefullProfileLinks/UsefullProfileLinkComponent.jsx";
 import ThreeColsLayout from "../layouts/ThreeColsLayout.jsx";
 import NewsContainer from "../components/newsContainer/NewsContainer.jsx";
+import Opportunity from "../components/opportunity/Opportunity.jsx";
 
 const HomePage = () => {
     return (
@@ -13,6 +14,7 @@ const HomePage = () => {
             <div className="col col-12 col-md-6 col-lg-5">Colonna da 5</div>
             <div className="col col-12 col-md-6 col-lg-4">
                 <NewsContainer />
+                <Opportunity />
             </div>
         </ThreeColsLayout>
     );
