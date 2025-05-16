84e6a715596744309641a4b371e00e637743d230
Danilo Scano <daniloscano@gmail.com>
2025-05-16 19:38:19 +0200

    main header on profile page init

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,10 +1,11 @@
 import TwoColsLayout from "../layouts/TwoColsLayout.jsx";
+import MainHeader from "../components/mainHeader/MainHeader.jsx";
 
 const ProfilePage = () => {
     return (
         <TwoColsLayout>
             <div className="col col-12 col-md-6 col-lg-8">
-                Colonna da 8
+                <MainHeader />
             </div>
             <div className="col col-12 col-md-6 col-lg-4">
                 Colonna da 4
